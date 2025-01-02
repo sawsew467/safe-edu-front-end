44cf509344b1c438ea196c16bb7c59c78fc826d3
Lê Đức Anh Phương <leducanhphuongdev@gmail.com>
2025-01-02 12:30:15 +0700

    quen xoa title cũ

--- src/app/(dashboard)/tin-tuc/[id]/chinh-sua/page.tsx
+++ src/app/(dashboard)/tin-tuc/[id]/chinh-sua/page.tsx
@@ -7,7 +7,6 @@ const UpdateLibraryPage = ({ params }: { params: Promise<{ id: string }> }) => {
 
   return (
     <div className="bg-white dark:bg-background p-4 rounded-xl border-[1px] border-stone-50 dark:border-stone-800">
-      <h3 className="text-2xl font-bold mb-4">Thay đổi danh sách</h3>
       <FormUpdateNews id={id} />
     </div>
   );
