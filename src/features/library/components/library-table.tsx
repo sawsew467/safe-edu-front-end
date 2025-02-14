878c7fddafd0dd3c12a48b7cf0d137f05c48c36c
Lê Đức Anh Phương <127871368+LeDucAnhPhuong@users.noreply.github.com>
2025-02-14 09:32:49 +0700

    sua lai cau truc (#11)

--- src/features/library/components/library-table.tsx
+++ src/features/library/components/library-table.tsx
@@ -10,29 +10,30 @@ import { useGetAllTopicQuery } from "@/features/topic/api";
 import { DataTopic } from "@/features/topic/topic.type";
 
 const LibraryTable = () => {
+  const { data: topics, isSuccess } = useGetAllTopicQuery({});
   const { librarys, isFetching } = useGetAllLibraryQuery(
     {},
     {
-      refetchOnMountOrArgChange: true,
+      skip: !isSuccess,
       selectFromResult: ({ data, isFetching }) => {
         return {
-          librarys: data?.items?.filter((item: Library) => item.isActive) ?? [],
+          librarys:
+            data?.items
+              ?.filter((item: Library) => item.isActive)
+              ?.map((item: Library) => ({
+                ...item,
+                topic_name: topics?.data?.find(
+                  (topic: DataTopic) => topic._id === item.topic_id,
+                )?.topic_name,
+              })) ?? [],
           isFetching,
         };
       },
+      refetchOnMountOrArgChange: true,
     },
   );
 
-  const { data: topics } = useGetAllTopicQuery({});
-
-  const data = librarys?.map((library: Library) => ({
-    ...library,
-    topic_name: topics?.data?.find(
-      (topic: DataTopic) => topic._id === library.topic_id,
-    )?.topic_name,
-  }));
-
-  return <DataTable columns={columns} data={data} isLoading={isFetching} />;
+  return <DataTable columns={columns} data={librarys} isLoading={isFetching} />;
 };
 
 export default LibraryTable;
