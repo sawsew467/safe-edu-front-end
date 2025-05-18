c1a3909b9a6d64ab1e2528daebf031ca19cc167d
khang <khangnguyen.030204@gmail.com>
2025-05-18 17:23:34 +0700

    feat: integrate visit API

---
+++ src/features/statistics/components/visit-static.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import { useVisitMutation } from '@/features/statistics/api';
+import React, { useEffect, useState } from 'react'
+
+function VisitStatic() {
+    const [visit] = useVisitMutation(); const [ip, setIp] = useState<string>("");
+
+    const getCurrentLocation = () => {
+                    if (navigator.geolocation) {
+                        navigator.geolocation.getCurrentPosition(
+                            (position) => {
+                                const { latitude, longitude } = position.coords;
+
+
+                            },
+                            (error) => {
+                            }
+                        );
+                    } else {
+                    }
+                };
+    useEffect(() => {
+        const fetchIpAndVisit = async () => {
+            try {
+                getCurrentLocation();
+                const response = await fetch('https://api4.ipify.org/?format=json',
+                    {
+                        method: 'GET',
+                        headers: {
+                            'Content-Type': 'application/json',
+                            'Accept': 'application/json',
+                        },
+                    }
+                );
+
+                const data = await response.json();
+                console.log(data);
+                
+                setIp(data.IPv4);
+                visit({ ipAddress: data.IPv4 });
+                console.log("[VisitStatic] User IP address:", data.IPv4);
+            } catch (error) {
+                console.error("Error fetching IP address:", error);
+            }
+        };
+        fetchIpAndVisit();
+    }, [visit]);
+    return null
+}
+
+export default VisitStatic
